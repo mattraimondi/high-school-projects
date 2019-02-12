47761950e32f1bc6ee4b826293ad7a67a48dc85f
Matthew <huggyraimondi@gmail.com>
2019-02-12 14:39:39 -0500

    add all of intro

---
+++ Intro to Computer Science/Unity 3D/Move a Cube/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    
+    public float moveSpeed;
+    
+    void Start ()
+    {
+        moveSpeed = 1f;
+    }
+
+    void Update ()
+    {
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+    }
+}
